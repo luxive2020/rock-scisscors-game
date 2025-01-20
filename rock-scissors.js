686dd2ba45bf4471add10140181749e8b3c4e493
Tajudeen Taiwo <tajudeentaiwo13@gmail.com>
2025-01-20 13:36:19 +0100

    rock paper scissors game UI implemented

--- rock-scissors.js
+++ rock-scissors.js
@@ -1,4 +1,12 @@
 console.log("Hello, World!");
+
+
+const computerBtn = document.querySelector("#button-computer");
+console.dir(computerBtn);
+const humanBtn = document.querySelector("#getHumanChoice");
+console.dir(humanBtn);
+const playBtn = document.querySelector("#playRound");
+
 let Choice;
 let ROCK = 'rock';
 let SCISSORS = 'scissors';
@@ -9,14 +17,18 @@ let computerScore = 0;
 
 //Function to Get Computer Choice.
 function getComputerChoice(){
+    let computerVal = ''
     randNum = Math.random()
     if (randNum <0.33){
-        console.log("paper");
+        computerVal = 'paper';
     } else if( randNum<0.67){
-        console.log("rock")
+        computerVal = 'rock'
     }else if(randNum <1){
-        console.log("scissors")
+        computerVal = 'scissors';
     }
+ 
+    return computerVal;
+    
 }
 
 //Function to get Human Choice.
@@ -25,55 +37,66 @@ function getHumanChoice(){
     humanChoice = humanChoice.trim().toLowerCase();
     if (humanChoice === ROCK){
         // console.log(`Your choice is ${ROCK}`)
-        return humanChoice;
+        return (humanChoice);
     } else if(humanChoice === SCISSORS){
         // console.log(`Your choice is ${SCISSORS}`)
-        return humanChoice;
+        return (humanChoice);
     } else if (humanChoice === PAPER){
         // console.log(`Your choice is ${PAPER}`)
-        return humanChoice;
+        return (humanChoice);
     }else{
         console.log('Your select option not available. Please try again!')
     }  
 }
 
 // Function to play the game for 1 round
-function playRound(humanChoice, computerChoice){
+function playRound(){
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
     if (humanChoice === SCISSORS || computerChoice === PAPER){
         humanScore++;
-        console.log(`You win ${SCISSORS} beats ${PAPER}`);
+        alert(`You win ${SCISSORS} beats ${PAPER}`);
     }else if(humanChoice=== PAPER || computerChoice=== ROCK){
-        console.log(`You lose ${ROCK} beats ${SCISSORS}`);
+        alert(`You lose ${ROCK} beats ${SCISSORS}`);
         computerScore++
     } else if( humanChoice === ROCK || computerChoice=== SCISSORS){
         console.log(`You win ${ROCK} beats ${SCISSORS}`);
         humanScore++
     }
     else{
-        console.log('You draw!')
+        alert('You draw!')
     }
+    
 };
+ 
 
+
+
+console.dir(computerBtn)
+computerBtn.value = 'paper';
+console.log(computerBtn.value)
+humanBtn.addEventListener('click', getHumanChoice);
+playBtn.addEventListener('click', playRound);
 
 // function to play the game for 5 rounds
-function playGame(){
-    let i;
-    for (i=0;  i<5; i++){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice(); 
-        playRound(humanSelection, computerSelection);
+// function playGame(){
+//     let i;
+//     for (i=0;  i<5; i++){
+//         const humanSelection = getHumanChoice();
+//         const computerSelection = getComputerChoice(); 
+//         playRound(humanSelection, computerSelection);
         
-    }
-    if (humanScore>computerScore){
-        console.log('Human wins the overall game')
-    }else if (humanScore === computerScore){
-        console.log('Draw!')
-    } else{
-        console.log('Computer wins the overall game')
-    }
+//     }
+//     if (humanScore>computerScore){
+//         console.log('Human wins the overall game')
+//     }else if (humanScore === computerScore){
+//         console.log('Draw!')
+//     } else{
+//         console.log('Computer wins the overall game')
+//     }
   
-}
-playGame()
+// }
+// playGame()
 
 //Make a Variable for each humman and computer selection
 
